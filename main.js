292d02204c2860f002dde90178dcf12669b97953
Kevin Abreu <kevinrabreu@hotmail.com>
2019-05-07 09:35:20 -0400

    revised the for loop in assignment 2

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ swap("Hello")
 //---------------------------------------------------------------------
 
 //FizzBuzz Assignment
-for (var i=1; i < 101; i++){
+for (var i=0; i <= 100; i++){
     if (i % 15 == 0) console.log("FizzBuzz");
     else if (i % 3 == 0) console.log("Fizz");
     else if (i % 5 == 0) console.log("Buzz");
